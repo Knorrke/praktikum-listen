a4824c4cdcb505fd869a8db09bc1c46b25e79b7d
Knorrke <knorr.b@gmx.de>
2020-03-30 10:55:29 +0200

    Warteschlange Struktur und Daten noch nicht getrennt

--- warteschlange/KUNDE.java
+++ warteschlange/KUNDE.java
@@ -1,5 +1,9 @@
 package warteschlange;
 
+/**
+ *  Die Kunden verwalten hier rekursiv eine Listenstruktur, 
+ *  d.h. jeder Kunde hat eine Referenz auf seinen Nachfolger
+ */
 class KUNDE
 {
     KUNDE() {
